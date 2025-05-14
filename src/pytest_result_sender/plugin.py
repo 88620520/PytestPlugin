4bb24eb49ad54d26aff1cb7f74894e152c201d68
jiangmaosong <18320429962@163.com>
2025-05-14 15:13:13 +0800

    更新了README.md，加上了项目的简单描述，利用 pdm add (插件名) -d 命令下载了black，isort，flack8等项目管理工具，更新了pyproject.toml 加入了项目管理工具的脚手架，便于一键检查代码质量（pdm check）

--- src/pytest_result_sender/plugin.py
+++ src/pytest_result_sender/plugin.py
@@ -1,11 +1,12 @@
 # conftest.py
 from datetime import datetime
 
+
 def pytest_configure():
     """在所有测试开始前执行"""
     print(f"\n{datetime.now()} pytest开始执行")
 
+
 def pytest_unconfigure():
     """在所有测试结束后执行"""
     print(f"\n{datetime.now()} pytest结束执行")
-
