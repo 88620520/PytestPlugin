2522a9315c492460e3c9d34506775d547c398d95
jiangmaosong <18320429962@163.com>
2025-05-15 16:24:42 +0800

    新增了test_001测试文件，加了3个测试用例，用于插件开发的校验，新增了pytest_collection_finish 用于统计需要执行的用例数量的一个hookspec

--- src/pytest_result_sender/plugin.py
+++ src/pytest_result_sender/plugin.py
@@ -1,12 +1,27 @@
 # conftest.py
-from datetime import datetime
+import pytest
+from datetime import datetime,timedelta
+
+data={}
+def pytest_collection_finish(session:pytest.Session):
+    data['total']=len(session.items)
+    print(f"要执行的用例数量 {data['total']}")
 
 
 def pytest_configure():
     """在所有测试开始前执行"""
-    print(f"\n{datetime.now()} pytest开始执行")
+    data['start_test']=datetime.now()
+    print(f"\n{data['start_test']} pytest开始执行")
 
 
 def pytest_unconfigure():
     """在所有测试结束后执行"""
-    print(f"\n{datetime.now()} pytest结束执行")
+    data['end_test']=datetime.now()
+    print(f"\n{data['end_test']} pytest结束执行")
+    # 计算时间差
+    data['time_stamp']=data['end_test']-data['start_test']
+    print(f"测试用例执行的时间{data['time_stamp']}")
+
+    assert timedelta(seconds=3) <= data['time_stamp'] <= timedelta(seconds=4)
+    assert data['total'] == 3
+
